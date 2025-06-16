d3d391bb0c4377d416d6b5464182e9903b6a6678
Creswa <dgros@seznam.cz>
2025-06-16 11:23:53 +0200

    No string var, clean, working

---
+++ frames.py
@@ -0,0 +1,96 @@
+import tkinter as tk
+from tkinter import ttk
+
+class TopFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.create_layout_and_widgets()
+        self.pack(expand = False, fill = "x")
+
+    def create_layout_and_widgets(self):
+        # layout
+        self.columnconfigure((0,1), weight = 1, uniform="a") #buď to i s placingem může být tady, nebo by se to dalo do jiné funkce a před všechno by se dalo self.name.grid(...)
+        self.columnconfigure(2, weight = 2)
+
+        # creating widgets
+        name = tk.Label(self, text = 'Name:', background = 'red', anchor = "w")
+        total_level = tk.Label(self, text = 'Level:', background = "purple", anchor = "w")
+
+        # placing widgets
+        name.grid(column = 0, sticky = "nsew")
+        total_level.grid(row = 0, column = 1, sticky = "nsew")
+
+class MidFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.create_layout_and_widgets()
+        self.pack(expand = True, fill = "both")
+    
+    def create_layout_and_widgets(self):
+        # layout
+        global SkillsFrame
+        SkillsFrame = tk.Frame(self)
+
+        # create widgets
+        SkillsFrame.pack(expand=True, fill="both")
+
+        # place widgets
+        SkillsFrame.pack(expand = True, fill = "both")
+
+class BottomFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.create_layout_and_widgets()
+        self.pack(expand = False, fill = "x")
+
+    def create_new_skill(self):
+        if SkillsFrame is not None:
+            NewSkill(SkillsFrame)
+
+    def create_layout_and_widgets(self):
+        # layout
+        self.columnconfigure(0, weight = 2)
+        self.columnconfigure((1,2), weight = 1, uniform="a")
+
+        # create widgets
+        skill_name = ttk.Entry(self)
+        add_skill_button = ttk.Button(self, text = 'Add skills')
+        delete_mode_button = ttk.Button(self, text = 'Delete mode')
+
+        # place widgets 
+        skill_name.grid(column = 0, sticky = "nsew")
+        add_skill_button.grid(row = 0, column = 1, sticky = "nsew")
+        delete_mode_button.grid(row = 0, column =2, sticky = "nsew")
+
+class NewSkill(ttk.Frame):
+        def __init__(self, parent):
+             super().__init__(parent)
+             self.create()
+             self.pack(expand = False, fill = "x")
+
+        def create(self):
+            # new skill frame
+            new_skill_frame = ttk.Frame(self, height = 50, borderwidth = 5, relief="solid")
+            new_skill_frame.pack_propagate(False)
+            new_skill_frame.pack(side = "top", fill = "x", expand = False)
+
+            # new skill frame grid
+            new_skill_frame.columnconfigure((0,1), weight = 2)
+            new_skill_frame.columnconfigure((2,3), weight = 1, uniform="a")
+            new_skill_frame.rowconfigure((0,1), weight=1)
+
+            #new_skill_name_input = parent.new_skill_name.get() # string variable for the new skill
+
+            # creating widgets
+            skill_name = ttk.Label(new_skill_frame, anchor="w", borderwidth=1, relief="solid")
+            level = ttk.Label(new_skill_frame, text="Level", anchor="w", borderwidth=1, relief="solid")
+            mninus_button = ttk.Button(new_skill_frame, text="+")
+            plus_button = ttk.Button(new_skill_frame, text="-")
+            level_bar = tk.Label(new_skill_frame, text="Level progress", anchor="center", borderwidth=2, relief="solid", background = "red")
+
+            #placement to grid
+            skill_name.grid(column = 0, sticky = "nsew")
+            level.grid(row = 0, column = 1, sticky = "nsew")
+            mninus_button.grid(row = 0, column = 2, sticky = "nsew")
+            plus_button.grid(row = 0, column = 3, sticky = "nsew")
+            level_bar.grid(row = 1, column = 0, columnspan = 4, sticky = "nsew") 
